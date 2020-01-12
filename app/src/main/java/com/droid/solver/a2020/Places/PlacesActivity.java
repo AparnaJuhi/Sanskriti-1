762c1997f70b7d743fbe21fae2571cc1ebb6e6a2
sihnitp <heritagenitp.2020@gmail.com>
2020-01-12 17:39:01 +0530

    first commit from aparna

---
+++ app/src/main/java/com/droid/solver/a2020/Places/PlacesActivity.java
@@ -0,0 +1,16 @@
+package com.droid.solver.a2020.Places;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.droid.solver.a2020.R;
+
+public class PlacesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_places);
+    }
+}
